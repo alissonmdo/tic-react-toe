ee08ea6b96bd4e4420da68fac2a4e13a28d4f346
Alisson Oliveira <git@alisson.dev>
2019-08-14 12:52:25 -0300

    Updates

---
+++ precache-manifest.f73f9d1d206ff742485c01f73dc58183.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a97a7c06b434ce09358670c681e89958",
+    "url": "/tic-react-toe/index.html"
+  },
+  {
+    "revision": "f35bac8dfa5c5c3042a7",
+    "url": "/tic-react-toe/static/css/main.b1a63161.chunk.css"
+  },
+  {
+    "revision": "a316500900215573f9ce",
+    "url": "/tic-react-toe/static/js/2.9e24791f.chunk.js"
+  },
+  {
+    "revision": "f35bac8dfa5c5c3042a7",
+    "url": "/tic-react-toe/static/js/main.8f49f2ff.chunk.js"
+  },
+  {
+    "revision": "000fd08e8c5f1ae51453",
+    "url": "/tic-react-toe/static/js/runtime~main.1c3e9060.js"
+  }
+]);
