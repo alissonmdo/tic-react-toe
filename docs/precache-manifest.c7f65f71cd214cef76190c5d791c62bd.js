23b554635e5d6bdddefac9974ac71db432fb0696
Alisson Oliveira <git@alisson.dev>
2019-08-14 12:35:45 -0300

    Change build folder name to docs

---
+++ docs/precache-manifest.c7f65f71cd214cef76190c5d791c62bd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b3b4628c992bf4d415e98ab79527842",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f35bac8dfa5c5c3042a7",
+    "url": "/static/css/main.b1a63161.chunk.css"
+  },
+  {
+    "revision": "a316500900215573f9ce",
+    "url": "/static/js/2.9e24791f.chunk.js"
+  },
+  {
+    "revision": "f35bac8dfa5c5c3042a7",
+    "url": "/static/js/main.8f49f2ff.chunk.js"
+  },
+  {
+    "revision": "1192af711d6a0d31385a",
+    "url": "/static/js/runtime~main.2239b7b0.js"
+  }
+]);
